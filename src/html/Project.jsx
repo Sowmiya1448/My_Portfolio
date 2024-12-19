1d344fecdf0ce3d6bc05a9dbff82c6184dafeea7
SOWMIYA S <sowmi625@gmail.com>
2024-12-19 12:27:02 +0530

    updated

--- src/html/Project.jsx
+++ src/html/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../css/project.css'
-import res from "../assets/res.png"
+import res from "../assets/res2.png"
 import port from '../assets/port.png'
 import lap from '../assets/lap.png'
 
@@ -24,20 +24,23 @@ const Project = () => {
 
             <img src={res} alt="res" />
             <h5>Restaurant Management System</h5>
-            <p>Technologies Used : React, CSS, Nodejs, Expressjs, MongoDB</p>
-            <button > <a href="#">View</a></button>
-
-          </div>
+            <p className='tech'>Technologies Used : React, CSS, Nodejs, Expressjs, MongoDB</p>
+            <p>Developed a Restaurant Management system with separate login pages for chefs and admins. 
+              Admin functionalities include Dashboard, ordering, billing, managing menus and employees, 
+              calculating monthly Salary based on Attendances.</p>
+        
+        </div>
 
           <div className='projects'>
 
             <img src={lap} alt="laptop" />
             <h5>Laptop Shop Management System</h5>
-            <p>Technologies Used : React, CSS, Nodejs, Expressjs, MongoDB</p>
+            <p className='tech'>Technologies Used : React, CSS, Nodejs, Expressjs, MongoDB</p>
+            <p>Built a web application using the MERN stack for managing laptops, stock, dealer details,
+               billing and payment details in a laptop shop.</p>
           
 
 
-            <button> <a href="#">View</a></button>
 
           </div>
 
@@ -47,11 +50,13 @@ const Project = () => {
 
             <img src={port} alt="portfolio" />
             <h5>My Portfolio</h5>
-            <p>Technologies Used : HTML, CSS, REACT</p>
+            <p className='tech'>Technologies Used : HTML, CSS, REACT</p>
+            <p>Built a personal portfolio website using HTML, CSS and React, 
+              showcasing my skills and projects.</p>
     
 
 
-            <button > <a href="#">View</a></button>
+           
 
           </div>
 
